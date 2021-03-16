075da69c12c1b759d577482f7930a562c20908d2
Cloelh <39408084+Cloelh@users.noreply.github.com>
2021-03-16 09:11:01 +0100

    first commit - structure code

---
+++ config/actions.php
@@ -0,0 +1,17 @@
+<?php
+// Voici la liste des actions possibles avec la page à charger associée
+
+$listeDesActions = array(
+    //vues
+    "" => "vues/home.php",
+    "home" => "vues/home.php",
+    "connexion" => "vues/connexion.php",
+    "myPost" => "vues/myPost.php",
+
+    //traitement
+    "register" => "traitement/register.php",
+    "login" => "traitement/login.php",
+    "logout" => "traitement/logout.php",
+)
+
+?>
